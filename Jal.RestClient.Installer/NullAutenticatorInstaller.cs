00f77793cb16ac50684e7f5a84c541bcc03a5a6b
Raul Naupari <raul.naupari@cignium.com>
2015-12-21 17:31:33 -0500

    Initial commit

---
+++ Jal.RestClient.Installer/NullAutenticatorInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Jal.RestClient.Impl;
+using Jal.RestClient.Interface;
+
+namespace Jal.RestClient.Installer
+{
+    public class NullAutenticatorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IAuthenticator>().ImplementedBy<NullAuthenticator>());
+        }
+    }
+}
