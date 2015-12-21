1747544a591860223e7a2eefd03d0d5dbdf67eb3
Raul Naupari <raul.naupari@cignium.com>
2015-12-21 11:49:20 -0500

    Initial commit

--- pack.bat
+++ pack.bat
@@ -1,9 +1,5 @@
-packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.HttpClient\Jal.HttpClient.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.HttpClient.Nuget
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.RestClient\Jal.RestClient.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.RestClient.Nuget
 
-packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.HttpClient.Installer\Jal.HttpClient.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.HttpClient.Nuget
-
-packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.HttpClient.Logger\Jal.HttpClient.Logger.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.HttpClient.Nuget
-
-packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.HttpClient.Logger.Installer\Jal.HttpClient.Logger.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.HttpClient.Nuget
+packages\NuGet.CommandLine.3.3.0\tools\nuget pack Jal.RestClient.Installer\Jal.RestClient.Installer.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.RestClient.Nuget
 
 pause;
