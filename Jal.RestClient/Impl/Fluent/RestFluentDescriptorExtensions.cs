ff88c21b2c4d5b097ed34bd478ee99f3fa6dab53
raul naupari <raul.naupari@cignium.com>
2016-12-14 14:20:36 -0500

    extension methods

--- Jal.RestClient/Impl/Fluent/RestFluentDescriptorExtensions.cs
+++ Jal.RestClient/Impl/Fluent/RestFluentDescriptorExtensions.cs
@@ -1,34 +1,54 @@
-﻿using Jal.RestClient.Interface.Fluent;
+﻿using System;
+using Jal.RestClient.Interface.Fluent;
 
 namespace Jal.RestClient.Impl.Fluent
 {
     public static class RestFluentDescriptorExtensions
     {
-        public static IRestAuthenticatorDescriptor AuthorizedByBearerToken(this IRestAuthenticatorDescriptor descriptor, string token)
+        public static IRestHeaderDescriptor AuthorizedByBearerToken(this IRestAuthenticatorDescriptor descriptor, string token)
         {
             var auth = new TokenAuthenticator(token, "Bearer");
 
-            descriptor.AuthorizedBy(auth.Authenticate);
-
-            return descriptor;
+            return descriptor.AuthorizedBy(auth.Authenticate);
         }
 
-        public static IRestAuthenticatorDescriptor AuthorizedByToken(this IRestAuthenticatorDescriptor descriptor, string token, string type)
+        public static IRestHeaderDescriptor AuthorizedByToken(this IRestAuthenticatorDescriptor descriptor, string token, string type)
         {
             var auth = new TokenAuthenticator(token, type);
 
-            descriptor.AuthorizedBy(auth.Authenticate);
-
-            return descriptor;
+            return descriptor.AuthorizedBy(auth.Authenticate);
         }
 
-        public static IRestAuthenticatorDescriptor AuthorizedByBasicHttp(this IRestAuthenticatorDescriptor descriptor, string user, string password)
+        public static IRestHeaderDescriptor AuthorizedByBasicHttp(this IRestAuthenticatorDescriptor descriptor, string user, string password)
         {
             var auth = new BasicHttpAuthenticator(user, password);
 
-            descriptor.AuthorizedBy(auth.Authenticate);
+            return descriptor.AuthorizedBy(auth.Authenticate);
+        }
 
-            return descriptor;
+        public static IRestMapDescriptor FormUrlEncodedData(this IRestContentDescriptor descriptor, string data)
+        {
+            return descriptor.Data(data, "application/x-www-form-urlencoded");
+        }
+
+        public static IRestMapDescriptor MultipartFormData(this IRestContentDescriptor descriptor, string data)
+        {
+            return descriptor.Data(data, "multipart/form-data");
+        }
+
+        public static IRestMapDescriptor XmlData(this IRestContentDescriptor descriptor, string data)
+        {
+            return descriptor.Data(data, "text/xml");
+        }
+
+        public static IRestMapDescriptor XmlData<T>(this IRestContentDescriptor descriptor, T data, Func<T, string> converter)
+        {
+            return descriptor.Data(data, converter, "text/xml");
+        }
+
+        public static IRestWhenDescriptor<T> MapXmlTo<T>(this IRestMapDescriptor descriptor, Func<string, T> converter)
+        {
+            return descriptor.MapTo(converter, "text/xml");
         }
     }
 }
